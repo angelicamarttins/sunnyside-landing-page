75cfd2f1ff27fee620b0b03ce7004358ce7e4a93
Angélica Martins <marttinsangelica@gmail.com>
2022-01-19 17:05:26 -0300

    Adjusted the folders and files in src folder. Tried to compose the landing page with several components

---
+++ src/pageComponents/menu/Menu.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Import images
+import Logo from '../assets/images/icons/logo.svg'
+
+function Menu() {
+    return (
+        <div className='menu'>
+            <img src={Logo} />
+        </div>
+    )
+}
+
+export default Menu;
---
+++ src/pageComponents/titleContainer/TitleContainer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function TitleContainer(props) {
+
+    <div className='title-container'>
+
+        <h1 className='title-container__title'>we are creatives</h1>
+
+        <img className='title-container__arrow-down' src={props.src} />
+        
+    </div>
+
+}
+
+export default TitleContainer;
