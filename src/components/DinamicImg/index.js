91794774fea907342a5328ec85957c27c663942e
Angélica Martins <marttinsangelica@gmail.com>
2022-02-03 09:57:47 -0300

    Finished the MenuDesktop component style

--- src/components/DinamicImg/index.js
+++ src/components/DinamicImg/index.js
@@ -1,20 +1,22 @@
 import "./style.scss";
 
 const DinamicImg = (props) => {
+  const { media, srcSet, src, alt } = props;
+  
   return (
     <div className="dinamic-img">
       <picture className="dinamic-img__picture">
         <source
           className="picture__src"
-          media={props.media}
-          srcSet={props.srcset}
-          alt={props.alt}
+          media={media}
+          srcSet={srcSet}
+          alt={alt}
         />
 
-        <img className="picture__img" src={props.src} alt={props.alt} />
+        <img className="picture__img" src={src} alt={alt} />
       </picture>
     </div>
   );
-}
+};
 
 export default DinamicImg;
