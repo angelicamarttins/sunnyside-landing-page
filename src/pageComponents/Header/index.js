7d57f97c985f5df5b6111ddb337245968b9d36dd
Angélica Martins <marttinsangelica@gmail.com>
2022-03-09 10:53:59 -0300

    feat: add conditional rendering in Header Component to decide which menu will display
    In Header Component it's necessary to render properly which menu should display to the user. In that
    way, if the viewport width is more than 750px, MenUDesktop should be rendered, if not, the
    MenuMobile will be displayed. Therefore, an UseState was created to store the viewport width and an
    UseEffect to update the useState. After it, a conditional rendering was add to decide which menu
    will be displayed.

--- src/pageComponents/Header/index.js
+++ src/pageComponents/Header/index.js
@@ -1,6 +1,8 @@
+import { useEffect, useState } from 'react'
+
 import './style.scss'
 
-// import MenuDesktop from '../../components/MenuDesktop'
+import MenuDesktop from '../../components/MenuDesktop'
 import MenuMobile from '../../components/MenuMobile'
 import DinamicImg from '../../components/DinamicImg'
 import TitleContainer from '../../components/TitleContainer'
@@ -9,11 +11,27 @@ import HeaderDesktop from '../../assets/images/desktop/image-header.jpg'
 import HeaderMobile from '../../assets/images/mobile/image-header.jpg'
 import ArrowDown from '../../assets/images/icons/icon-arrow-down.svg'
 
+const getWindowWidthDimension = () => {
+	const width = window.innerWidth
+	return width
+}
+
 const Header = () => {
+	const [windowWidthDimension, setWindowWidthDimension] = useState(
+		getWindowWidthDimension()
+	)
+
+	useEffect(() => {
+		function handleResize() {
+			setWindowWidthDimension(getWindowWidthDimension())
+		}
+
+		window.addEventListener('resize', handleResize)
+	}, [windowWidthDimension])
+
 	return (
 		<header className='header'>
-			{/* <MenuDesktop /> */}
-			<MenuMobile />
+			{windowWidthDimension < 750 ? <MenuMobile /> : <MenuDesktop />}
 			<div className='header__title'>
 				<DinamicImg
 					src={HeaderDesktop}
